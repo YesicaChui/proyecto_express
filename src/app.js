0b19e2bd0c744bc72e8c3e06df0479c0385ef87c
YesicaChui <yesicachuic@gmail.com>
2023-04-22 11:52:31 -0500

    configurando handlebars con la ruta /

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import express, { json, urlencoded } from "express";
 import productRouter from './routes/products.router.js'
 import cartRouter from './routes/carts.router.js'
-
+import handlebars from 'express-handlebars'
+import  routerViews from './routes/views.router.js'
+import __dirname from './utils.js'
 const app = express()
 app.use(json())
 app.use(urlencoded({extended:true}))
+//configuracion handlebars
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname+'/views')
+app.set('view engine', 'handlebars')
 
+
+app.use('/', routerViews)
 app.use('/products',productRouter)
 app.use('/carts',cartRouter)
 
